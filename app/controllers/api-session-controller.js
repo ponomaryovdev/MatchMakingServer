8ab26ccd1cf198493ef4f3112ed43d549d5461e4
av.ponomarev <ponomaryovdev@gmail.com>
2022-12-28 12:17:52 +0300

    Signed-off-by: av.ponomarev <ponomaryovdev@gmail.com>

--- app/controllers/api-session-controller.js
+++ app/controllers/api-session-controller.js
@@ -30,7 +30,7 @@ const runNewSessionInstance = function(port)
         proeccesInstances[instanceProcess.pid] = instanceProcess;
     }
     if(utils.isLinux()){
-        instanceProcess = exec('/home/ponomarevav/Documents/matchmaking/MatchMakingServer/mastergame/metaverseserver/LinuxServer/MultiplayerTemplate/Binaries/Linux/MultiplayerTemplateServer -log');
+        instanceProcess = exec(`/home/ponomarevav/Documents/matchmaking/MatchMakingServer/mastergame/metaverseserver/LinuxServer/MultiplayerTemplate/Binaries/Linux/MultiplayerTemplateServer -log ?port=${port}`);
     }
     return 0;
 }
